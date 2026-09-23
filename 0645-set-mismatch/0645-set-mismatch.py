92da8017961b0dbd362392d06ecbc1eaf3fb75f6
NagaRaju Mullapudi <nagarajumullapudi321@gmail.com>
2026-09-23 15:43:47 +0530

    Time: 28 ms (22.1%), Space: 20.7 MB (46.99%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -3,15 +3,16 @@ class Solution:
         d = {}
         for i in nums:
             d[i] = d.get(i,0) + 1
-
-        dulpicate = 0
+        duplicate = 0
         missing = 0
-        for i in range(1,len(nums) + 1):
+        for i in range(1, len(nums) + 1):
             if d.get(i,0) == 2:
                 duplicate = i
             elif d.get(i,0) == 0:
                 missing = i
         return(duplicate, missing)
+
+        
 
         
         
